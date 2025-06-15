3292f21d101a0451ba522b291c23a6430fa662c7
TianfeiJi <iteacoder@foxmail.com>
2025-06-15 21:33:00 +0800

    update: run_docker_command方法不再记录日志

--- Deploy-Agent/src/utils/docker_util.py
+++ Deploy-Agent/src/utils/docker_util.py
@@ -1,9 +1,4 @@
 import subprocess
-from config.log_config import get_logger
-
-logger = get_logger()
-
-
 
 # Docker命令执行路径，需根据宿主机挂载配置一致
 DOCKER_PATH = "/usr/bin/docker"
@@ -11,6 +6,5 @@ DOCKER_PATH = "/usr/bin/docker"
 # 安全执行Docker命令的辅助函数
 def run_docker_command(args: list) -> str:
     command = [DOCKER_PATH] + args
-    logger.info(f"执行Docker命令：{' '.join(command)}")
     output = subprocess.check_output(command, encoding="utf-8")
     return output
