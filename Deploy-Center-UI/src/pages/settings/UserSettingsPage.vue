dd128452322c9e04e6c879a1d985539ba95783eb
TianfeiJi <iteacoder@foxmail.com>
2025-07-04 15:38:41 +0800

    polish(ui): 优化用户状态的显示

--- Deploy-Center-UI/src/pages/settings/UserSettingsPage.vue
+++ Deploy-Center-UI/src/pages/settings/UserSettingsPage.vue
@@ -3,19 +3,10 @@
     <q-card class="q-pa-xl" style="width: 100%">
       <div class="row items-center justify-between q-mb-xl">
         <div class="title">用户管理</div>
-        <el-button type="primary" @click="showAddDialog = true"
-          >新增用户</el-button
-        >
+        <el-button type="primary" @click="showAddDialog = true">新增用户</el-button>
       </div>
 
-      <el-table
-        :data="userList"
-        style="width: 100%"
-        border
-        stripe
-        highlight-current-row
-        row-key="id"
-      >
+      <el-table :data="userList" style="width: 100%" border stripe highlight-current-row row-key="id">
         <el-table-column label="ID" prop="id" width="60" />
 
         <el-table-column label="用户名" min-width="70">
@@ -34,17 +25,12 @@
           <template #default="{ row }">{{ row.permissions }}</template>
         </el-table-column>
 
-        <el-table-column label="状态" min-width="50">
+        <el-table-column label="状态" min-width="30">
           <template #default="{ row }">
-           <el-switch
-              v-model="row.status"
-              :active-value="'ENABLED'"
-              :inactive-value="'DISENABLED'"
-              inline-prompt
-              active-text="启用"
-              inactive-text="禁用"
-              @change="(val: string) => onToggleUserStatus(row, val)"
-            />
+            <el-tag :type="row.status === 'ENABLED' ? 'success' : 'info'" disable-transitions class="tag-action"
+              @click="onToggleUserStatus(row, row.status === 'ENABLED' ? 'DISENABLED' : 'ENABLED')">
+              {{ row.status === 'ENABLED' ? '启用' : '禁用' }}
+            </el-tag>
           </template>
         </el-table-column>
 
@@ -60,35 +46,28 @@
     <el-dialog v-model="showEditDialog" title="编辑用户" width="500px">
       <el-form :model="editUser" label-width="80px">
         <el-form-item label="用户名">
-          <el-input v-model="editUser.username"/>
+          <el-input v-model="editUser.username" />
         </el-form-item>
 
         <el-form-item label="昵称">
-          <el-input v-model="editUser.nickname"/>
+          <el-input v-model="editUser.nickname" />
         </el-form-item>
 
         <el-form-item label="密码">
-          <el-input
-            v-model="editUser.password"
-            :type="showPassword ? 'text' : 'password'"
-            placeholder="请输入密码"
-          >
+          <el-input v-model="editUser.password" :type="showPassword ? 'text' : 'password'" placeholder="请输入密码">
             <template #append>
-              <q-icon
-                :name="showPassword ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="showPassword = !showPassword"
-              />
+              <q-icon :name="showPassword ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                @click="showPassword = !showPassword" />
             </template>
           </el-input>
         </el-form-item>
 
         <el-form-item label="角色">
-          <el-input v-model="editUser.role"/>
+          <el-input v-model="editUser.role" />
         </el-form-item>
 
         <el-form-item label="权限">
-          <el-input v-model="editUser.permissions"/>
+          <el-input v-model="editUser.permissions" />
         </el-form-item>
       </el-form>
 
@@ -182,8 +161,8 @@ const onToggleUserStatus = async (row: User, newStatus: string) => {
 
   const ajaxResult = await changeUserStatus(row.id, newStatus);
 
-  if (ajaxResult.status == "success"){
-    Notify.create({type: 'positive', message: `${action}成功`});
+  if (ajaxResult.status == "success") {
+    Notify.create({ type: 'positive', message: `${action}成功` });
   } else {
     // 取消操作，回滚状态
     row.status = newStatus === 'ENABLED' ? 'DISENABLED' : 'ENABLED';
@@ -247,7 +226,18 @@ onMounted(() => {
 .rounded {
   border-radius: 6px;
 }
+
 .text-grey {
   color: #888;
+}
+
+.tag-action {
+  cursor: pointer;
+  user-select: none;
+  transition: all 0.2s;
+}
+.tag-action:hover {
+  filter: brightness(1.1);
+  box-shadow: 0 0 0 1px #dcdfe6;
 }
 </style>
